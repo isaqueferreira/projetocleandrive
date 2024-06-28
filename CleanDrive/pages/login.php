36f46bcf9956fa4015695eb2411e23ec86956a74
isaqueferreira <144289972+isaqueferreira@users.noreply.github.com>
2024-06-27 23:06:19 -0300

    Update login.php

--- CleanDrive/pages/login.php
+++ CleanDrive/pages/login.php
@@ -1,5 +1,6 @@
+
 <?php
-include '../includes/db.php'; // Verifique se o arquivo db.php está configurado corretamente
+include '../includes/db.php';
 session_start();
 
 function page_specific_css() {
@@ -9,24 +10,19 @@ function page_specific_css() {
 include '../includes/header.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['email']) && isset($_POST['password'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        $sql = 'SELECT * FROM users WHERE email = ?';
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$email]);
-        $user = $stmt->fetch();
+    $sql = 'SELECT * FROM users WHERE email = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email]);
+    $user = $stmt->fetch();
 
-        if ($user && password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['id'];
-            header('Location: ../pages/fleet.php'); // Corrigido o caminho do redirecionamento
-            exit(); // Certifique-se de que não há mais saída após o redirecionamento
-        } else {
-            $error = 'Invalid login credentials';
-        }
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        header('Location: fleet.php');
     } else {
-        $error = 'Please fill in both email and password fields';
+        $error = 'Invalid login credentials';
     }
 }
 ?>
@@ -40,8 +36,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <form method="post">
                 <label>Email: <br><input type="email" name="email" placeholder="Digite seu e-mail" required></label><br>
                 <label>Senha: <br><input type="password" name="password" placeholder="Digite sua senha" required></label><br>
-                <button class="botao-login" type="submit">Login</button>
-                <p>Não tem conta? <a href="../pages/register.php">Cadastre-se</a></p> <!-- Corrigido o caminho do link de registro -->
+                <button class="botao-login"type="submit">Login</button>
+                <p>Não tem conta? <a href="..\pages\register.php">Cadastre-se</a></p>
             </form>
         </div>   
     </div>  
